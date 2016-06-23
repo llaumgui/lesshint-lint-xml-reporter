d1d8cc4d31a68a7dd3a0ece2c6a2e6585ce5ba63
Guillaume Kulakowski <guillaume@kulakowski.fr>
2016-06-23 10:42:51 +0200

    Rename reporter

---
+++ lint-xml.js
@@ -0,0 +1,43 @@
+/* global module */
+
+'use strict';
+module.exports = {
+    name: 'lint-xml',
+    report: function report (results) {
+        var resultsByfiles = {};
+        var output = '';
+
+        console.log('<?xml version="1.0" encoding="utf-8"?>');
+        console.log('<lint>');
+
+        results.forEach(function (result) {
+            output = '\t<issue';
+            if (result.severity === 'error') {
+                output += ' severity="error"';
+            } else {
+                output += ' severity="warning"';
+            }
+            if (result.line) {
+                output += ' line="' + result.line + '"';
+            }
+            if (result.column) {
+                output += ' char="' + result.column + '"';
+            }
+
+            output += ' reason="' + result.linter + ': ' + result.message.replace(/"/g, '&quot;') + '"';
+
+            if (resultsByfiles[result.file] === undefined) {
+                resultsByfiles[result.file] = '';
+            }
+            resultsByfiles[result.fullPath] += output + ' />\n';
+        });
+
+        Object.keys(resultsByfiles).forEach(function (file) {
+            console.log('<file name="' + file + '">');
+            console.log(resultsByfiles[file]);
+            console.log('</file>');
+        });
+
+        console.log('</lint>');
+    }
+};
